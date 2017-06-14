e878cb670c6908169fe9bd4cbb1bac0053e6e0fa
woniu <275949034@qq.com>
2017-06-14 10:20:54 +0800

    使用kotlin的委托属性，将application做成一个全局的非空单例，作为全局的context使用

--- app/src/main/java/com/woniu/gank/kotlin/GankApp.kt
+++ app/src/main/java/com/woniu/gank/kotlin/GankApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.woniu.gank.kotlin.di.AppComponent
 import com.woniu.gank.kotlin.di.AppModule
 import com.woniu.gank.kotlin.di.DaggerAppComponent
+import kotlin.properties.Delegates
 
 /**
  * @author woniu
@@ -22,8 +23,15 @@ class GankApp : Application() {
                 .build()
     }
 
+    //使用kotlin的委托属性特性，讲GankApp做一个全局非空的单例化context
+    companion object {
+        //委托特性详解参见：http://www.tuicool.com/articles/AVNNNfF
+        var instance: GankApp by Delegates.notNull<GankApp>()
+    }
+
     override fun onCreate() {
         super.onCreate()
+        instance = this;
     }
 
 }
