2485158069295541068c15b2f42774631ebb4e8f
woniu <275949034@qq.com>
2017-06-30 21:52:25 +0800

    在全局的AppComponent中添加DataManager的声明，以便后面做全局的依赖注入

--- app/src/main/java/com/woniu/gank/kotlin/di/AppComponent.kt
+++ app/src/main/java/com/woniu/gank/kotlin/di/AppComponent.kt
@@ -1,5 +1,6 @@
 package com.woniu.gank.kotlin.di
 
+import com.woniu.gank.kotlin.data.DataManager
 import dagger.Component
 import javax.inject.Singleton
 
@@ -14,5 +15,7 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = arrayOf(AppModule::class, ClientModule::class))
 interface AppComponent {
+
+    fun getDataManager(): DataManager
 
 }
